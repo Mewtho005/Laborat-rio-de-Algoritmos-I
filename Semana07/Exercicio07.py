70792ec5aa5ae1d15d2e621ab56ef1721fa7f630
Pedro Renato <pedroprochnow6@gmail.com>
2026-09-08 19:50:52 -0300

    Create Exercicio07.py

---
+++ Semana07/Exercicio07.py
@@ -0,0 +1,28 @@
+pessoa = 1
+soma = 0
+maior = 0
+menor = 999999
+todos = 0
+
+while pessoa <= 20:
+    vezes = int(input("Quantas vezes por semana toma chimarrão? "))
+
+    soma = soma + vezes
+
+    if vezes > maior:
+        maior = vezes
+
+    if vezes < menor:
+        menor = vezes
+
+    if vezes >= 7:
+        todos = todos + 1
+
+    pessoa = pessoa + 1
+
+media = soma / 20
+
+print("Média:", media)
+print("Maior quantidade:", maior)
+print("Menor quantidade:", menor)
+print("Tomam todos os dias:", todos)
