dfd3b5a3c3cecc9a25cc7b56219fd81eac1b6c21
Pedro Renato <pedroprochnow6@gmail.com>
2026-09-08 21:30:52 -0300

    Create Exercicio06.py

---
+++ Semana08/Exercicio06.py
@@ -0,0 +1,9 @@
+print("Ervateira Recanto - Tabela de Preços")
+
+pacotes = 1
+
+while pacotes <= 30:
+    valor = pacotes * 18.90
+    print(f"{pacotes} pacote(s) - R$ {valor}")
+    pacotes += 1
+  
